4323b9ec2683aa8d4302d3d612dabbbfe65898d6
Septicuss <vbalaban55@gmail.com>
2026-03-31 16:05:30 +0300

    test: implement game data tests

--- src/tests/game_data_test.py
+++ src/tests/game_data_test.py
@@ -1,11 +1,31 @@
+import json
 import unittest
+
+from data.game_data import GameData
 
 default_data_path = "src/data/data.json"
 
 class TestGameData(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def test_default_game_data_valid(self):
+        try:
+            GameData.load_from_file(default_data_path)
+        except ValueError as error:
+            self.fail("default game data failed validation: " +  str(error))
 
+    def test_missing_reference_raises_error(self):
+        # Recipe refers to non-existing "berry"
+        data = {
+            "recipes": [{"id": "test", "name": "test", "time": 10, "recipe": [{"id": "berry"}]}],
+        }
+        self.assertRaisesRegex(ValueError, "berry", GameData.load_from_json, json.dumps(data))
 
-    pass
+    def test_duplicate_id_raises_error(self):
+        # Both crops have the same id
+        data = {
+            "crops": [
+                {"id": "wheat", "name": "wheat", "time": 5},
+                {"id": "wheat", "name": "wheat", "time": 5}
+            ],
+        }
+        self.assertRaisesRegex(ValueError, "duplicate ids", GameData.load_from_json, json.dumps(data))
